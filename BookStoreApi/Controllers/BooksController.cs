cedea96dbf3e94e1ded22f1210c34fb9fa2040dd
Yasar Yigit Usta <yigitusta112@gmail.com>
2025-11-02 01:13:31 +0300

    first commit

---
+++ BookStoreApi/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private static readonly List<string> books = new()
+        {
+            "CleanCode","C# in Depth","Desing Patterns"
+        };
+
+        [HttpGet]
+        public IActionResult Get() => Ok(books);
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string title)
+        {
+            books.Add(title);
+            return CreatedAtAction(nameof(Get), new { }, books);
+        }
+        [HttpGet("count")]
+        public IActionResult Count() => Ok(books.Count);
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= books.Count)
+            {
+                return NotFound();
+            }
+            books.RemoveAt(id);
+            return NoContent();
+        }
+    }
+}
